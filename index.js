76f72a3fa5e194b84eebcfebb5f9945655c12c78
anjanipodomi <anjanidihapsari@gmail.com>
2025-10-28 14:41:47 +0700

    menambahkan endpoint POST /komik dan konfigurasi koneksi database Sequelize

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log('Server Started');
+    })
+})
+    .catch((err) => {
+        console.log(err);
+})
+
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try{
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+});
+
